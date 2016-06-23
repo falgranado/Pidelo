b6f0f9af7f1e647bd1b5f0ae7daaf71e62f82f1d
Francisco A. Granado Santos <falgranado@gmail.com>
2016-06-23 06:13:40 -0400

    Added some includes
    Added footer.php and made simple signup form logic.

---
+++ web/php/signup.php
@@ -0,0 +1,17 @@
+<?php
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $address = $_POST['address'];
+  $name =$_POST['name'];
+  $lname = $_POST['lname'];
+  $phone = $_POST['phone'];
+
+If isset($username) && isset($email) && isset($password){
+  echo 'Registration complete'
+  //to do
+}else {
+  //todo
+}
+
+?>
